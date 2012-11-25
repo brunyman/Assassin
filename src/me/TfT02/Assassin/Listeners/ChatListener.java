a791f66a6ba271a141c7a6c434823ee203c57009
TfT_02 <tft_02@hotmail.com>
2012-11-25 23:43:37 +0100

    Message nearby players when you become an Assassin

--- src/me/TfT02/Assassin/Listeners/ChatListener.java
+++ src/me/TfT02/Assassin/Listeners/ChatListener.java
@@ -1,5 +1,38 @@
 package me.TfT02.Assassin.Listeners;
 
-public class ChatListener {
+import me.TfT02.Assassin.Assassin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatListener implements Listener {
+	Assassin plugin;
+
+	public ChatListener(final Assassin instance) {
+		plugin = instance;
+	}
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+		if (event.isCancelled()){
+			return;
+		}
+		Player player = event.getPlayer();
+		String msg = event.getMessage();
+
+		if (msg == null)
+			return;
+double chatDistance = 25;
+		// Chat Distance Stuff
+		if (chatDistance > 0)
+			for (Player players : plugin.getServer().getOnlinePlayers()) {
+				if (players.getWorld() != player.getWorld() || players.getLocation().distance(player.getLocation()) > chatDistance) {
+					event.getRecipients().remove(players);
+				}
+			}
+
+//		event.setFormat(eventFormat);
+	}
 }
