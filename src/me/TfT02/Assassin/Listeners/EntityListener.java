92e9aa7da595f8106cd2be7eb110755c4907ffc7
TfT_02 <tft_02@hotmail.com>
2012-11-28 01:02:02 +0100

    PVP Handling

--- src/me/TfT02/Assassin/Listeners/EntityListener.java
+++ src/me/TfT02/Assassin/Listeners/EntityListener.java
@@ -1,8 +1,17 @@
 package me.TfT02.Assassin.Listeners;
 
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Tameable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import me.TfT02.Assassin.Assassin;
+import me.TfT02.Assassin.util.PlayerData;
 
 public class EntityListener implements Listener {
 
@@ -10,5 +19,52 @@ public class EntityListener implements Listener {
 
 	public EntityListener(final Assassin instance) {
 		plugin = instance;
+	}
+
+	private final PlayerData data = new PlayerData(plugin);
+
+	/**
+	 * Monitor EntityDamageByEntity events.
+	 * 
+	 * @param event The event to monitor
+	 */
+	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+//        if (event instanceof FakeEntityDamageByEntityEvent)
+//            return;
+
+		if (event.getDamage() <= 0)
+			return;
+
+		Entity attacker = event.getDamager();
+		Entity defender = event.getEntity();
+
+		if (attacker.hasMetadata("NPC") || defender.hasMetadata("NPC"))
+			return; // Check if either players is are Citizens NPCs
+
+		if (attacker instanceof Projectile) {
+			attacker = ((Projectile) attacker).getShooter();
+		} else if (attacker instanceof Tameable) {
+			AnimalTamer animalTamer = ((Tameable) attacker).getOwner();
+
+			if (animalTamer instanceof Entity) {
+				attacker = (Entity) animalTamer;
+			}
+		}
+
+		if (defender instanceof Player) {
+			Player defendingPlayer = (Player) defender;
+
+			if (!defendingPlayer.isOnline()) {
+				return;
+			}
+
+			if (attacker instanceof Player) {
+				if (data.bothNeutral(defendingPlayer, (Player) attacker)) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+		}
 	}
 }
