acc0869205773c4d1b65af39f849eaf966321c29
TfT_02 <tft_02@hotmail.com>
2012-11-28 01:00:10 +0100

    This was the wrong way around

--- src/me/TfT02/Assassin/Listeners/TagListener.java
+++ src/me/TfT02/Assassin/Listeners/TagListener.java
@@ -1,7 +1,7 @@
 package me.TfT02.Assassin.Listeners;
 
 import me.TfT02.Assassin.Assassin;
-import me.TfT02.Assassin.PlayerData;
+import me.TfT02.Assassin.util.PlayerData;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -10,27 +10,26 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.kitteh.tag.PlayerReceiveNameTagEvent;
 
-public class TagListener
-
-implements Listener {
+public class TagListener implements Listener {
 
 	Assassin plugin;
 
 	public TagListener(Assassin instance) {
 		plugin = instance;
 	}
+
 	private final PlayerData data = new PlayerData(plugin);
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onNameTag(PlayerReceiveNameTagEvent event) {
 		Player player = event.getPlayer();
-		if (data.isAssassin(player)){
+		Player namedPlayer = event.getNamedPlayer();
+		if (data.isAssassin(namedPlayer)) {
 			event.setTag(ChatColor.DARK_RED + "[ASSASSIN]");
-			System.out.println("Changed player tag to [ASSASSIN] for " +player.getName());
-		}
-		else {
-			event.setTag(ChatColor.WHITE + player.getDisplayName());
-			System.out.println("Reset player tag for " +player.getName());
+			System.out.println("Changed player tag to [ASSASSIN] for " + namedPlayer.getName());
+		} else {
+			event.setTag(ChatColor.WHITE + namedPlayer.getDisplayName());
+			System.out.println("Reset player tag for " + namedPlayer.getName());
 		}
 //		for(Player p : getServer().getOnlinePlayers()){
 //		if (event.getPlayer().getName().equals(player)) {
