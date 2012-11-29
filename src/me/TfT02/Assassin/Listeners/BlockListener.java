aef8360a0897a7c9418cd7c4dd98785af51fde7c
TfT_02 <tft_02@hotmail.com>
2012-11-29 23:41:37 +0100

    Prevent placing masks

--- src/me/TfT02/Assassin/Listeners/BlockListener.java
+++ src/me/TfT02/Assassin/Listeners/BlockListener.java
@@ -1,14 +1,35 @@
 package me.TfT02.Assassin.Listeners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 
 import me.TfT02.Assassin.Assassin;
+import me.TfT02.Assassin.util.itemNamer;
 
 public class BlockListener implements Listener {
 	Assassin plugin;
 
 	public BlockListener(final Assassin instance) {
 		plugin = instance;
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		Block block = event.getBlock();
+		ItemStack itemHand = event.getItemInHand();
+		int id = block.getTypeId();
+		if (id == 35) {
+			String item = itemNamer.getName(itemHand);
+			String mask = ChatColor.DARK_RED + "Assassin Mask";
+			if (item == null) {
+			} else if (item.equalsIgnoreCase(mask)) {
+				event.setCancelled(true);
+			}
+		}
 	}
 	//TODO Perhaps prevent block breaks from Assassins?
 	// This might be a simple way to prevent griefers to become anonymous and destroying stuff
