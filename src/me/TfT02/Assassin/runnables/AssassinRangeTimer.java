08194afac43cedda47d2eb9b179aa50a068791a3
TfT_02 <tft_02@hotmail.com>
2012-11-29 23:42:03 +0100

    Trying to handle the 'Assassin near' messages

--- src/me/TfT02/Assassin/runnables/AssassinRangeTimer.java
+++ src/me/TfT02/Assassin/runnables/AssassinRangeTimer.java
@@ -1,13 +1,13 @@
 package me.TfT02.Assassin.runnables;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import me.TfT02.Assassin.Assassin;
 import me.TfT02.Assassin.util.PlayerData;
 
-
-public class AssassinRangeTimer  implements Runnable{
+public class AssassinRangeTimer implements Runnable {
 	Assassin plugin;
 
 	public AssassinRangeTimer(final Assassin instance) {
@@ -15,22 +15,26 @@ public class AssassinRangeTimer  implements Runnable{
 	}
 
 	private final PlayerData data = new PlayerData(plugin);
+
 	@Override
-	public void run() {		
-		for (Player players : plugin.getServer().getOnlinePlayers()) {
-			if (data.isAssassin(players)){
-				checkIfAssassinNear(players);
-			}
-		}
+	public void run() {
+		checkIfAssassinNear();
 	}
 
-	public void checkIfAssassinNear(Player assassin){
+	public void checkIfAssassinNear() {
 		double distance = 250;
-		if (distance > 0)
-			for (Player players : plugin.getServer().getOnlinePlayers()) {
-				if (players.getWorld() != assassin.getWorld() || players.getLocation().distance(assassin.getLocation()) > distance) {
-					players.sendMessage(ChatColor.DARK_RED + "ASSASSIN SIGHTED!");
+		for (Player players : Bukkit.getServer().getOnlinePlayers()) {
+			for (String assassins : data.getOnlineAssassins()) {
+				if (players.getName().equals(assassins)) {
+					Player assassin = Bukkit.getServer().getPlayer(assassins);
+					System.out.println("Checking if Assassin near.");
+					if (distance > 0)
+						if (players.getWorld() != assassin.getWorld() || players.getLocation().distance(assassin.getLocation()) > distance) {
+							players.sendMessage(ChatColor.DARK_RED + "ASSASSIN SIGHTED!");
+
+						}
 				}
 			}
+		}
 	}
 }
