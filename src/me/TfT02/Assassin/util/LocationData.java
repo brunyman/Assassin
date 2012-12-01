4d6333c21c4218acf2a487e8fdfdd5af2129be44
TfT_02 <tft_02@hotmail.com>
2012-12-01 14:55:26 +0100

    Trying to steralize location data

---
+++ src/me/TfT02/Assassin/util/LocationData.java
@@ -0,0 +1,38 @@
+package me.TfT02.Assassin.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class LocationData {
+	   
+    private Location location;
+   
+    public LocationData(Location location) {
+        this.location = location;
+    }
+   
+    public String convertToString() {
+        String locationData = location.getWorld().getName() + ", " + String.valueOf(location.getX()) + ", " + String.valueOf(location.getY()) + ", " + String.valueOf(location.getZ());
+       
+        return locationData;
+    }
+   
+    public static LocationData convertFromString(String locationData) {
+        locationData = locationData.replaceAll("\\s", "");
+        String[] locationArray = locationData.split(",");
+        String locationWorldName = locationArray[0];
+        double locX = Double.parseDouble(locationArray[1]);
+        double locY = Double.parseDouble(locationArray[2]);
+        double locZ = Double.parseDouble(locationArray[3]);
+       
+        return new LocationData(new Location(Bukkit.getWorld(locationWorldName), locX, locY, locZ));
+    }
+   
+    public Location getLocation() {
+        return location;
+    }
+   
+    public void setLocation(Location l) {
+        this.location = l;
+    }
+}
