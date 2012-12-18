1e9c71325d92aa021f03331da00709a5bd2db07a
TfT_02 <tft_02@hotmail.com>
2012-12-19 00:34:40 +0100

    Message scrambler finished

--- src/me/TfT02/Assassin/util/MessageScrambler.java
+++ src/me/TfT02/Assassin/util/MessageScrambler.java
@@ -18,29 +18,55 @@ public class MessageScrambler {
 	 * This will randomly add ChatColor.MAGIC so messages will get scrambled.
 	 * @param msg Message that will get scrambled
 	 * @return Scrambled message
-	*/
+	 */
 	public String Scrambled(String msg) {
-		int length = msg.length();
-		int halfLength = length / 2;
-		int minScramLength = length / 4;
-		int scrambleLength1 = random.nextInt(5) + minScramLength;
-		int scrambleLength2 = random.nextInt(5) + minScramLength;
-		int start1 = 1 + random.nextInt(halfLength / 2);
-		int end1 = start1 + scrambleLength1;
-		int start2 = 2 + end1 + random.nextInt(halfLength);
-		int end2 = start2 + scrambleLength2;
-		
+		int halfLength;
+
 		StringBuilder sb = new StringBuilder(msg);
-		if (start2 > length) {
-			start2 = end1 + 3;
+		int start1;
+		int end1;
+		int start2;
+		int end2;
+		int scrambleLength1;
+		int scrambleLength2;
+
+		int length = msg.length();
+		if (length == 0){
+			//This is not possible, but hey! You never know, right? :D
+		}else if (length == 1) {
+			int scramble = random.nextInt(100);
+			if (scramble > 50) {
+				sb.insert(0, ChatColor.MAGIC);
+			}
+		}else if (length <= 3) {
+			start1 = random.nextInt(length);
+			end1 = random.nextInt(length + 1);
+			sb.insert(start1, ChatColor.MAGIC);
+			sb.insert(end1 + 2, ChatColor.RESET);
+		}else if (length >= 4) {
+			halfLength = (int) length / 2;
+			scrambleLength1 = random.nextInt(5) + 3;
+			scrambleLength2 = random.nextInt(5) + 3;
+			start1 = random.nextInt(halfLength);
+			end1 = scrambleLength1 + start1;
+			start2 = random.nextInt(halfLength) + end1;
+			end2 = scrambleLength2 + start2;
+			if (end2 > length) {
+				end2 = length;
+			}
+			if (start2 > length) {
+				start2 = length;
+			}
+			sb.insert(start1, ChatColor.MAGIC);
+			sb.insert(end1 + 2, ChatColor.RESET);
+			sb.insert(start2 + 4, ChatColor.MAGIC);
+			sb.insert(end2 + 6, ChatColor.RESET);
+			if (plugin.debug_mode) {
+				System.out.println("DEBUG INFORMATION:");
+				System.out.println(start1 + "-" + end1 + " | " + scrambleLength1);
+				System.out.println(start2 + "-" + end2 + " | " + scrambleLength2);
+			}
 		}
-		if (end2 > length) {
-			end2 = length;
-		}
-		sb.insert(start1, ChatColor.MAGIC);
-		sb.insert(end1 + 2, ChatColor.RESET);
-		sb.insert(start2 + 4, ChatColor.MAGIC);
-		sb.insert(end2 + 6, ChatColor.RESET);
 		String scrambled = sb.toString();
 		return scrambled;
 	}
