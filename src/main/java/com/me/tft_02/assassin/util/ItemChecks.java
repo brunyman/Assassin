4c23eff2e49e25577be50ed512d2b8041215c814
TfT_02 <tft_02@hotmail.com>
2013-09-22 01:17:35 +0200

    Use Material instead of ID's here

--- src/main/java/com/me/tft_02/assassin/util/ItemChecks.java
+++ src/main/java/com/me/tft_02/assassin/util/ItemChecks.java
@@ -1,25 +1,31 @@
 package com.me.tft_02.assassin.util;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemChecks {
 
     public boolean isMask(ItemStack is) {
-        int id = is.getTypeId();
-        if (id == 35) {
-            ItemMeta im = is.getItemMeta();
-            String name = im.getDisplayName();
-            String mask = ChatColor.DARK_RED + "Assassin Mask";
-            if (name == null) {
-                return false;
-            }
+        Material material = is.getType();
 
-            if (name.equalsIgnoreCase(mask)) {
-                return true;
-            }
+        if (material != Material.WOOL) {
+            return false;
         }
+
+        ItemMeta im = is.getItemMeta();
+        String name = im.getDisplayName();
+        String mask = ChatColor.DARK_RED + "Assassin Mask";
+
+        if (name == null) {
+            return false;
+        }
+
+        if (name.equalsIgnoreCase(mask)) {
+            return true;
+        }
+
         return false;
     }
 }
