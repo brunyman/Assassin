794f2a6e2976d8623770d9d9ec6660a8873e53be
TfT_02 <tft_02@hotmail.com>
2014-08-22 23:54:42 -0700

    Now using PreTombCreateEvent

--- src/main/java/com/me/tft_02/assassin/hooks/GhostsListener.java
+++ src/main/java/com/me/tft_02/assassin/hooks/GhostsListener.java
@@ -6,12 +6,12 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.me.tft_02.assassin.util.player.UserManager;
-import com.me.tft_02.ghosts.events.tomb.TombCreateEvent;
+import com.me.tft_02.ghosts.events.tomb.PreTombCreateEvent;
 
 public class GhostsListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onCreateTomb(TombCreateEvent event) {
+    public void onCreateTomb(PreTombCreateEvent event) {
         Player player = event.getPlayer();
 
         if (UserManager.getPlayer(player).isAssassin()) {
